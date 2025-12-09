4a21818c6c534358a9840c0b92d7d4cead166869
SimonLiu <simonncliu@starlux-airlines.com>
2025-12-09 16:22:49 +0800

    temp

--- Chapter1/1-3/main_alphalens_analysis_for_multiple_factors_with_weighted_rank.py
+++ Chapter1/1-3/main_alphalens_analysis_for_multiple_factors_with_weighted_rank.py
@@ -11,7 +11,7 @@ chap1_utils.finlab_login()
 
 # 涵蓋 2017 第一季到 2020 第四季的財報數據
 analysis_period_start_date = "2017-05-16"
-analysis_period_end_date = "2021-05-15"
+analysis_period_end_date = "2025-12-09"
 
 top_N_stocks = chap1_utils.get_top_stocks_by_market_value(
     excluded_industry=[
@@ -22,6 +22,7 @@ top_N_stocks = chap1_utils.get_top_stocks_by_market_value(
     ],
     pre_list_date="2017-01-03",
 )
+
 # 獲取指定股票代碼列表在給定日期範圍內的每日收盤價資料。
 # 對應到財報資料時間 2017-Q1~2020-Q4
 close_price_data = chap1_utils.get_daily_close_prices_data(
@@ -29,6 +30,8 @@ close_price_data = chap1_utils.get_daily_close_prices_data(
     start_date=analysis_period_start_date,
     end_date=analysis_period_end_date,
 )
+# print(close_price_data)
+
 # 列舉和收益呈正相關的因子（根據先前單因子分析結果）
 pos_corr_factors = [
     "營業利益",
@@ -52,6 +55,7 @@ for factor in pos_corr_factors:
         .assign(factor_name=factor)
     )
     factors_data_dict[factor] = factor_data
+<<<<<<< Updated upstream
 # 根據各個因子（欄位：value）對股票進行排序，排序後的結果存儲在 ranked_factors_data_dict 字典中，字典的鍵是因子名稱，值為排名結果。
 rank_factors_data_dict = {}
 for factor in factors_data_dict:
@@ -89,3 +93,45 @@ for pair in combined_df_dict:
         periods=(1,),
     )
     create_full_tear_sheet(alphalens_factor_data)
+=======
+print(factors_data_dict)
+# # 根據各個因子（欄位：value）對股票進行排序，排序後的結果存放在 ranked_factors_data_dict 字典中，字典的鍵是因子名稱，值為排名結果。
+# rank_factors_data_dict = {}
+# for factor in factors_data_dict:
+#     rank_factors_data_dict[factor] = chap1_utils.rank_stocks_by_factor(
+#         factor_df=factors_data_dict[factor],
+#         positive_corr=True,
+#         rank_column="value",
+#         rank_result_column="rank",
+#     )
+
+# # 根據多個因子的排名和對應的權重，計算加權排名後．再依據加權排名結果將股票進行排序
+# pos_corr_factor_pair = list(combinations(pos_corr_factors, 5))
+# print(f"總計有 {len(pos_corr_factors)} 組五因子組合")
+
+# # combined_df_dict 用來儲存每個五因子組合的排續結果。
+# combined_df_dict = {}
+# for pair in pos_corr_factor_pair:
+#     combined_df_dict[pair] = chap1_utils.calculate_weighted_rank(
+#         ranked_dfs=[
+#             rank_factors_data_dict[pair[0]],
+#             rank_factors_data_dict[pair[1]],
+#             rank_factors_data_dict[pair[2]],
+#             rank_factors_data_dict[pair[3]],
+#             rank_factors_data_dict[pair[4]],
+#         ],
+#         weights=[0.2, 0.2, 0.2, 0.2, 0.2], # 等權重
+#         positive_corr=True,
+#         rank_column="rank",
+#     ).set_index(['datetime', 'asset'])
+
+# # 使用 Alphalens 進行因子分析
+# for pair in combined_df_dict:
+#     print(f"pair: {pair}")
+#     alphalens_factor_data = get_clean_factor_and_forward_returns(
+#         factor=combined_df_dict[pair].squeeze(),
+#         prices=close_price_data,
+#         periods=(1,),
+#     )
+#     create_full_tear_sheet(alphalens_factor_data)
+>>>>>>> Stashed changes
